72544067bc5e83e33f8470468401502aa9ffb1b0
Wekiban <wes.banyard@googlemail.com>
2021-01-22 19:48:33 +0100

    initial commit

---
+++ src/components/MemeGenerator.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+class MemeGenerator extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      topText: '',
+      bottomText: '',
+      randomImg: 'http://i.imgflip.com/1bij.jpg',
+      allMemeImgs: [],
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const randomNumber = Math.floor(
+      Math.random() * this.state.allMemeImgs.length
+    );
+    const randomImgURL = this.state.allMemeImgs[randomNumber].url;
+
+    this.setState({
+      randomImg: randomImgURL,
+    });
+  }
+
+  componentDidMount() {
+    fetch('https://api.imgflip.com/get_memes')
+      .then((response) => response.json())
+      .then((response) => this.setState({ allMemeImgs: response.data.memes }));
+  }
+  render() {
+    return (
+      <div>
+        <form action="" className="meme-form" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="topText"
+            value={this.state.topText}
+            onChange={this.handleChange}
+            placeholder="Bottom Text"
+          />
+          <input
+            type="text"
+            name="bottomText"
+            value={this.state.bottomText}
+            onChange={this.handleChange}
+            placeholder="Top Text"
+          />
+          <button>Gen</button>
+        </form>
+        <div className="meme">
+          <img src={this.state.randomImg} alt="" />
+          <h2 className="top">{this.state.topText}</h2>
+          <h2 className="bottom">{this.state.bottomText}</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MemeGenerator;
